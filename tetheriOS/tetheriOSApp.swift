f09fada81af8cfcd0dd1e072601bab8f1f3cc88a
Daksh Shahani <dakshitshahani@gmail.com>
2026-04-14 18:25:44 -0700

    Initial Commit

---
+++ tetheriOS/tetheriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  tetheriOSApp.swift
+//  tetheriOS
+//
+//  Created by Daksh Shahani on 2026-04-14.
+//
+
+import SwiftUI
+
+@main
+struct tetheriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
